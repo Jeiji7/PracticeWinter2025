1459777b18e772693b4f53f8f4a883bc99cc321f
Jeiji7 <egorpopelyuk43@gmail.com>
2025-01-20 15:07:12 +0300

    основные элементы добавлены

--- PracticeWinter2025/Pages/Authorization.xaml.cs
+++ PracticeWinter2025/Pages/Authorization.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PracticeWinter2025.db;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,21 @@ namespace PracticeWinter2025.Pages
         public Authorization()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_Enter(object sender, RoutedEventArgs e)
+        {
+            string name = LoginTB.Text.Trim();
+            string firstName = PassWordPS.Password.Trim();
+
+            if (LoginTB.Text == "0000" && PassWordPS.Password == "0000")
+                NavigationService.Navigate(new Pages.NavigationPage());
+            else
+            {
+                MessageBox.Show("Мы не нашли ваши данные в системе, попробуйте зайти снова");
+                LoginTB.Text = "";
+                PassWordPS.Password = "";
+            }
         }
     }
 }
